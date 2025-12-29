69202225e95f91d49a3107cc6690f48cc95150a6
ponomoly-moin <hyunjun.joo@themoin.com>
2025-12-29 18:15:44 +0900

    로봇 추가

---
+++ src/pages/landing-a.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function LandingA() {
+  return (
+    <>
+      <Head>
+        <title>Landing A</title>
+        <meta name="description" content="Landing A Page" />
+      </Head>
+      <main>
+        <h1>Landing A</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/not-landing-1.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function NotLanding1() {
+  return (
+    <>
+      <Head>
+        <title>Not Landing 1</title>
+        <meta name="description" content="Not Landing 1 Page" />
+      </Head>
+      <main>
+        <h1>Not Landing 1</h1>
+      </main>
+    </>
+  );
+}
